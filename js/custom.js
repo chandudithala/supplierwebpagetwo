8d8f2d4fa1ec6d0a78f6f3a93b74201132211a8d
chandu dithala <45012300+chandudithala@users.noreply.github.com>
2023-07-14 12:41:27 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,21 @@
-
+// Mobile products menu Function
+$('.cat_link .product_link_arrow').on('click', function() {
+    $('.cat_open').slideToggle();
+    $(this).toggleClass('active');
+    $(this).parent().toggleClass('active');
+});
+// Mobile products sub menu Function
+$('.sub_menu').click(function () { 
+    var $ul = $(this).siblings('ul');
+    $('.active1').removeClass('active1');   
+    $(this).addClass('active1');
+    $(this).parent().toggleClass('active1');
+    if ($ul.length > 0) {
+        $ul.slideToggle(600);
+        $(".sub_menu_drop").not($ul).slideUp(400);
+        return false;
+    }
+});
 //////////// Product Slider
 $('.responsive').slick({
     dots: true,
@@ -33,11 +50,6 @@ $('.responsive').slick({
     ]
 });
 
-// Mobile Catogiries Function
-$('.cat_link').on('click', function() {
-    $('.cat_open').slideToggle();
-    $(this).toggleClass('active');
-  });
 
 
 
